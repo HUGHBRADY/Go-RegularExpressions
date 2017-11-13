6c9faa6b07b650018fb52c9c19c38f96e1ffae34
Hugh <Hugh>
2017-11-13 20:22:20 +0000

    Recognizes 'father'

--- src/Eliza.go
+++ src/Eliza.go
@@ -7,6 +7,7 @@ package main
 import "math/rand"
 import "time"
 import "fmt"
+import "regexp"
 
 func ElizaResponse(input string) string {
 
@@ -17,17 +18,26 @@ func ElizaResponse(input string) string {
 		"Why do you say that?",
 	}
 
-	// Return a random string from responses array
+	// Checks if "father" is a word in the string
+	dad, _ := regexp.MatchString("(?i)\\bfather\\b", input)
+
+	// if input contains "father", return
+	if (dad) {
+		return("Why don’t you tell me more about your father?")
+	}
+
+	// Will return a random string from responses array otherwise
 	return responses[rand.Intn(len(responses))]
+	
 }
 
 func main() {
-	// Seed used to randomize eliza response
+	// Seed used to randomize Eliza response
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	// Print the input to console
+	// Then, print Eliza's response to the string you pass to her
 	fmt.Println("People say I look like both my mother and father.")
-	// Print Eliza's response to the string you pass to her
 	fmt.Println(ElizaResponse("People say I look like both my mother and father.")) 
 
 	fmt.Println("\nFather was a teacher.")
