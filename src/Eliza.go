d45d3c580f7bb6dc19a1f8a5fa9913a45542e252
HUGH BRADY - Student <G00338260@gmit.ie>
2017-11-15 10:57:16 +0000

    Eliza can now capture 'I am'

--- src/Eliza.go
+++ src/Eliza.go
@@ -13,17 +13,25 @@ func ElizaResponse(input string) string {
 
 	// Array of strings 
 	responses := []string{
+		"Why do you say that?",
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
 	}
 
-	// Checks if "father" is a word in the string
-	dad, _ := regexp.MatchString("(?i)\\bfather\\b", input)
+	// Compile a regular expression into a RegExp object used for matching text
 
+	// Checks if "father" is a word in the string
 	// if input contains "father", return
-	if (dad) {
-		return("Why don’t you tell me more about your father?")
+	// (?i) flags the condition as case insensitive
+	if dad, _ := regexp.MatchString(`(?i)\\bfather\\b`, input); dad {
+		return "Why don’t you tell me more about your father?"
+	}
+
+	// Checks if input begins with “I am" and capture part of the input
+	iam := regexp.MustCompile(`I am ([^.!?]*)[.!?]?`)
+	if iam.MatchString(input) {
+		return (iam.ReplaceAllString(input, "How do you know you are $1?"))		
 	}
 
 	// Will return a random string from responses array otherwise
@@ -37,18 +45,31 @@ func main() {
 
 	// Print the input to console
 	// Then, print Eliza's response to the string you pass to her
-	fmt.Println("People say I look like both my mother and father.")
-	fmt.Println(ElizaResponse("People say I look like both my mother and father.")) 
+	fmt.Println("Input: " + " People say I look like both my mother and father.")
+	fmt.Println("Output: " + ElizaResponse("People say I look like both my mother and father.")) 
 
-	fmt.Println("\nFather was a teacher.")
-	fmt.Println(ElizaResponse("Father was a teacher."))
+	fmt.Println("\nInput: " + " Father was a teacher.")
+	fmt.Println("Output: " + ElizaResponse("Father was a teacher."))
 
-	fmt.Println("\nI was my father’s favourite.")
-	fmt.Println(ElizaResponse("I was my father’s favourite."))
+	fmt.Println("\nInput: " + " I was my father’s favourite.")
+	fmt.Println("Output: " + ElizaResponse("I was my father’s favourite."))
 
-	fmt.Println("\nI'm looking forward to the weekend.")
-	fmt.Println(ElizaResponse("I'm looking forward to the weekend."))
+	fmt.Println("\nInput: " + " I am looking forward to the weekend.")
+	fmt.Println("Output: " + ElizaResponse("I'm looking forward to the weekend."))
 
-	fmt.Println("\nMy grandfather was French!")
-	fmt.Println(ElizaResponse("My grandfather was French!"))
+	fmt.Println("\nInput: " + " My grandfather was French!")
+	fmt.Println("Output: " + ElizaResponse("My grandfather was French!"))
+
+	fmt.Println("\nInput: " + "I am happy.")
+	fmt.Println("Output: " + ElizaResponse("I am happy."))
+	
+	fmt.Println("\nInput: " + "I am not happy with your responses.")
+	fmt.Println("Output: " + ElizaResponse("I am not happy with your responses."))
+
+	fmt.Println("\nInput: " + "I am not sure that you understand the effect that your questions are having on me.")
+	fmt.Println("Output: " + ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
+
+	fmt.Println("\nInput: " + "I am supposed to just take what you’re saying at face value?")	
+	fmt.Println("Output: " + ElizaResponse("I am supposed to just take what you’re saying at face value?"))
+		
 }
